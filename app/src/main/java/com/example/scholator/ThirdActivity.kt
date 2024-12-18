40bc91339c0d3ad2acca3e25fc3ef5ee5d154e0a
HarshithaRajashriHB <harshitha.rajashri@gmail.com>
2024-12-18 16:20:27 +0530

    authentication

--- app/src/main/java/com/example/scholator/ThirdActivity.kt
+++ app/src/main/java/com/example/scholator/ThirdActivity.kt
@@ -2,6 +2,8 @@ package com.example.scholator
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,10 +15,42 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_third)
+
+        // Handle system window insets for edge-to-edge display
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Set up Gender Spinner
+        val genderSpinner: Spinner = findViewById(R.id.gender_spinner)
+        val genderAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.gender_options, // Ensure you have defined this in strings.xml
+            android.R.layout.simple_spinner_item
+        )
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        genderSpinner.adapter = genderAdapter
+
+        // Set up Caste Spinner
+        val casteSpinner: Spinner = findViewById(R.id.caste_spinner)
+        val casteAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.caste_options, // Ensure you have defined this in strings.xml
+            android.R.layout.simple_spinner_item
+        )
+        casteAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        casteSpinner.adapter = casteAdapter
+
+        // Set up Disability Spinner
+        val disabilitySpinner: Spinner = findViewById(R.id.disability_spinner)
+        val disabilityAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.disability_options, // Ensure you have defined this in strings.xml
+            android.R.layout.simple_spinner_item
+        )
+        disabilityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        disabilitySpinner.adapter = disabilityAdapter
     }
 }
